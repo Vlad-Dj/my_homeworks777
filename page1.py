920e10170afd1f37a492b2a0942d79b1dcc2f336
Vlad-Dj <138291407+Vlad-Dj@users.noreply.github.com>
2023-07-07 11:51:02 +0300

    Create page1.py

---
+++ page1.py
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Page 1</title>
+</head>
+<body>
+ <a href="page2.html" target="_blank">Ссылка на страницу 2</a>
+ <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident beatae in, saepe blanditiis dolore autem porro officia error perferendis vero, quod aliquid. Alias possimus blanditiis quod autem ipsam, cupiditate asperiores.
+ </p>
+ <form action="">
+    <label for="field_1" class="lead">Введите имя:</label>
+    <input type="text" id="field_1" class="form-control form-control-lg"><br>
+    <label for="field_2" class="lead">Согласен на обработку данных:</label>
+    <input type="checkbox" id="field_2" class="form-check-input" checked><br>
+    <label for="field_3" class="lead">Введите пароль:</label>
+    <input type="password" id="field_3"class="form-control form-control-sm"><br>
+    <label for="field_4" class="lead">Отправьте отзыв:</label>
+    <textarea type="text" id="field_4"></textarea><br>
+    <input type="button" value="Отправить" class="btn btn-outline-warning"><br>
+
+ </form>
+
+    
+
+</body>
+</html>
